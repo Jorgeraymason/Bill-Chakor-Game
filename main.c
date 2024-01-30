954a8dbb25de024459dd8a66a7933cd69a2e32e6
Jorgeraymason <111918707+Jorgeraymason@users.noreply.github.com>
2024-01-30 11:15:27 -0800

    Update main.c
    MUCH BETTER LOGIC NOW TO PRINT THROUGH THE STORY

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 // BILL CHAKOR, THE TEXT BASED GAME
 // Credits: JORGERAYMASON
 
+// All library declartions
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -14,9 +15,11 @@ void exitGame();
 
 void chapterSelect();
 
-int makeChoice(); 
+int makeChoice();
 
 void waitForInput();
+
+void printStory(char** text);
 
 void introScene();
 
@@ -28,12 +31,11 @@ void chapter3TechnicalDifficulties();
 
 void chapter4DivineSolance();
 
-
 // Main function, contains the main menu
 int main()
 {
     // Input variable for user input
-    int input; 
+    int input;
 
     // Prints the logo of ASCII Bill
     printf("%s\n\n", asciiBill);
@@ -45,7 +47,7 @@ int main()
     scanf("%d", &input);
 
     // Switch case based on user input
-    switch(input)
+    switch (input)
     {
     case 1:
         chapter1ReptillianAuthority();
@@ -74,30 +76,33 @@ void waitForInput()
 
     char userInput = getchar();
 
-    if (userInput == 'Q'|| userInput == 'q')
+    if (userInput == 'Q' || userInput == 'q')
     {
         main();
     }
 }
 
 // Called every time user has to make a choice for a decistion in the story.
-int makeChoice(){
+int makeChoice()
+{
     int option = 0;
 
     printf("What do you choose?");
     scanf("%d", &option);
-    
+
     return option;
 }
 
 // Exits the game
-void exitGame(){
+void exitGame()
+{
     printf("Exiting Game...");
     exit(0);
 }
 
 // Allows user to select chapter to start from, or revisit
-void chapterSelect(){
+void chapterSelect()
+{
     int input;
 
     printf("Which chapter would you like ot visit? (Enter chapter number):\n");
@@ -128,9 +133,26 @@ void chapterSelect(){
     }
 }
 
-//Intro scene that starts game.
+// function prints the story text
+void printStory(char** text){
+
+    int textSize = sizeof(text) / sizeof(text[0]);
+
+    int i;
+
+    for (int i = 0; i < textSize; i++)
+    {
+        printf("%d: %s", i, text[i]);
+
+        waitForInput();
+
+        i++;
+    }
+}
+
+// Intro scene that starts game.
 void introScene()
-{ 
+{
     // Print the intro scene text
     char text[] = "\nIn the beginning, Bill Chakor is hired by Tensen Law firm in Roxford. He relocated to Roxford because of his newfound work. A taxi drops him off in front of the Roxford Hotel.\nBill steps out of the car and thinks to himself:\nBill: Is this the place? I guess so. Looks like Ill be staying here until I can see Jason tomorrow.\nBill walks into the hotel, where he is then greeted by the clerk at the front desk.\nHotel Clerk: Welcome, You must be Bill! You had a reservation. Tensen told me about your arrival. You are on the 11th floor, 7th room. All expenses covered. Enjoy your stay.";
 
@@ -155,7 +177,7 @@ void introScene()
     waitForInput();
 
     printf("Bill: Yeah. Kris would’ve been an….. asset about now. Anyways, tell me. What’s this case that came up on the news?\nThe two glance over at the TV, as the news comes on and explains the tragic scenario.");
-    
+
     chapter1ReptillianAuthority();
 }
 
@@ -163,71 +185,29 @@ void introScene()
 void chapter1ReptillianAuthority()
 {
     // Story text split across lines in an array
-    
-    //char *chapter1Characters[] = {"Zoey", "Bill", "Jason", "Newt", "LaShawnya"};
+
+    // char *chapter1Characters[] = {"Zoey", "Bill", "Jason", "Newt", "LaShawnya"};
 
     printf("**Chapter 1: Reptiallian Authority**\n**********************************\n");
 
     printf("%s", asciiNewt);
 
-    int textSize =  sizeof(chapter1StoryText) / sizeof(chapter1StoryText[0]);
-
-    for (int i = 0; i <= textSize; i++)
-    {
-        printf("%s", chapter1StoryText[i]);
-
-        waitForInput();
-
-        i++;
-    
-        if (i == 10)
-        {
-            int choice = 0;
-
-            printf("1.%s\n2.%s\n3. %s", chapter1StoryText[9], chapter1StoryText[10], chapter1StoryText[11]);
-
-            scanf("%d", &choice);
-
-            if (choice == 1)
-            {
-                i = 13;
-                //printf("%s", chapter1StoryText[i]);
-                break;
-            }
-            else if (choice == 2)
-            {
-                i = 14;
-                //printf("%s", chapter1StoryText[i]);
-                break;
-            }
-            else if (choice == 3)
-            {
-                i = 15;
-                //printf("%s", chapter1StoryText[i]);
-                break;
-            }
-        }
-        else
-        {
-            printf("%s", chapter1StoryText[i]);
-
-            waitForInput();
-
-            i++;
-        }
-    }
+    printStory(chapter1StoryText);
 }
 
-void chapter2HighStakes() {
-   // Code
-   printf("Hi");
+void chapter2HighStakes()
+{
+    // Code
+    printf("Hi");
 }
 
-void chapter3TechnicalDifficulties() {
-   // Code
-   printf("Hi");
+void chapter3TechnicalDifficulties()
+{
+    // Code
+    printf("Hi");
 }
 
-void chapter4DivineSolance() {
+void chapter4DivineSolance()
+{
     printf("Hi");
 }
