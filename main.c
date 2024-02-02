d4ba8c69d8cbc23b0123a9f7f320e2ca9beceaba
Jorgeraymason <111918707+Jorgeraymason@users.noreply.github.com>
2024-02-02 08:43:02 -0800

    Update main.c
    I RESTARTED ALL OVER. The original logic sucked....

--- main.c
+++ main.c
@@ -6,208 +6,50 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+
+// Inclue addtional Files 
 #include "story.c"
+#include "game.c"
 #include "ASCII.c"
 
 // Function prototype declarations
+int mainMenu();
 
-void exitGame();
-
-void chapterSelect();
-
-int makeChoice();
-
-void waitForInput();
-
-void printStory(char** text);
-
-void introScene();
-
-void chapter1ReptillianAuthority();
-
-void chapter2HighStakes();
-
-void chapter3TechnicalDifficulties();
-
-void chapter4DivineSolance();
+void chapter1ReptillainAuthority();
 
 // Main function, contains the main menu
 int main()
 {
-    // Input variable for user input
-    int input;
+    mainMenu();
 
-    // Prints the logo of ASCII Bill
-    printf("%s\n\n", asciiBill);
-
-    // Main menu text
-    printf("|*************|\nBill Chakor\n|*************|\nStart Game[1]\n|*************|\nLoad Game [2]\n|*************|\nChapter Select [3]\n||*************|\nExit Game[4]\n");
-
-    // Prompts user for input
-    scanf("%d", &input);
-
-    // Switch case based on user input
-    switch (input)
+    switch(mainMenu())
     {
     case 1:
-        chapter1ReptillianAuthority();
-        break;
-    case 2:
-        printf("Nothing here yet");
-        break;
-    case 3:
-        chapterSelect();
-        break;
-    case 4:
-        exitGame();
+        printf("Welcome to the game!\n\n");
+        chapter1ReptillainAuthority();
         break;
     default:
-        printf("Invalid input");
+        printf("Not cool");
         break;
     }
 
     return 0;
 }
 
-// Waits for user input each time to read next line.
-void waitForInput()
-{
-    printf("\n\nPress Enter to continue...\n\n");
+// Prints the Main Menu. NOT THE SAME AS MAIN. 
+int mainMenu(){
 
-    char userInput = getchar();
+    // Prints the logo of ASCII Bill
+    printf("%s\n\n", asciiBill);
 
-    if (userInput == 'Q' || userInput == 'q')
-    {
-        main();
-    }
-}
+    // Main menu text
+    printf("|*************|\nBill Chakor\n|*************|\nStart Game[1]\n");
 
-// Called every time user has to make a choice for a decistion in the story.
-int makeChoice()
-{
-    int option = 0;
-
-    printf("What do you choose?");
-    scanf("%d", &option);
-
-    return option;
-}
-
-// Exits the game
-void exitGame()
-{
-    printf("Exiting Game...");
-    exit(0);
-}
-
-// Allows user to select chapter to start from, or revisit
-void chapterSelect()
-{
+    // Input variable for user input
     int input;
 
-    printf("Which chapter would you like ot visit? (Enter chapter number):\n");
+    // Prompts user for input
+    scanf("%d\n", &input);
 
-    printf("Chapter 1: Reptillian Authority\n%s\n\n", asciiNewt);
-    printf("Chapter 2: High Stakes\n%s\n\n", asciiRicchi);
-    printf("Chapter 3: Technical Difficulties\n\n%s\n\n", asciiTakahashi);
-    printf("Chapter 4: Divine Solace\n\n%s\n\n", asciiKora);
-
-    scanf("%d", &input);
-
-    switch (input)
-    {
-    case 1:
-        chapter1ReptillianAuthority();
-        break;
-    case 2:
-        chapter2HighStakes();
-        break;
-    case 3:
-        chapter3TechnicalDifficulties();
-        break;
-    case 4:
-        chapter4DivineSolance();
-        break;
-    default:
-        break;
-    }
-}
-
-// function prints the story text
-void printStory(char** text){
-
-    int textSize = sizeof(text) / sizeof(text[0]);
-
-    int i;
-
-    for (int i = 0; i < textSize; i++)
-    {
-        printf("%d: %s", i, text[i]);
-
-        waitForInput();
-
-        i++;
-    }
-}
-
-// Intro scene that starts game.
-void introScene()
-{
-    // Print the intro scene text
-    char text[] = "\nIn the beginning, Bill Chakor is hired by Tensen Law firm in Roxford. He relocated to Roxford because of his newfound work. A taxi drops him off in front of the Roxford Hotel.\nBill steps out of the car and thinks to himself:\nBill: Is this the place? I guess so. Looks like Ill be staying here until I can see Jason tomorrow.\nBill walks into the hotel, where he is then greeted by the clerk at the front desk.\nHotel Clerk: Welcome, You must be Bill! You had a reservation. Tensen told me about your arrival. You are on the 11th floor, 7th room. All expenses covered. Enjoy your stay.";
-
-    printf("%s", text);
-
-    waitForInput();
-
-    printf("Bill nods and thanks the clerk. He heads to his room and gets settled in. He immediately checks his laptop and cell phone for any updates from Tensen.\nHe has one unread message from Tensen that reads: Welcome to Roxford, Bill.\nWe covered your hotel expenses as discussed. That’s just one of the benefits for working at our firm. I’ll see you tomorrow. - Jason'' \nBill closes his laptop and goes to sleep for the night.");
-
-    waitForInput();
-
-    printf("The next day, Bill goes out to meet with Jason at Tensen.");
-
-    waitForInput();
-
-    printf("Bill: (Going in for a handshake, enthusiastic) Jason! It’s been some time. New place, new office, same problems. Just like old times, huh?");
-
-    waitForInput();
-
-    printf("Jason: (Shakes hands, grinning) Ah, Bill. I’m so forever thankful the day Kris told me about you. But yeah like you said, new place, same problems");
-
-    waitForInput();
-
-    printf("Bill: Yeah. Kris would’ve been an….. asset about now. Anyways, tell me. What’s this case that came up on the news?\nThe two glance over at the TV, as the news comes on and explains the tragic scenario.");
-
-    chapter1ReptillianAuthority();
-}
-
-// 1st chapter of the game. The slimy racist and narcissist, Newt Manson.
-void chapter1ReptillianAuthority()
-{
-    // Story text split across lines in an array
-
-    // char *chapter1Characters[] = {"Zoey", "Bill", "Jason", "Newt", "LaShawnya"};
-
-    printf("**Chapter 1: Reptiallian Authority**\n**********************************\n");
-
-    printf("%s", asciiNewt);
-
-    printStory(chapter1StoryText);
-}
-
-void chapter2HighStakes()
-{
-    // Code
-    printf("Hi");
-}
-
-void chapter3TechnicalDifficulties()
-{
-    // Code
-    printf("Hi");
-}
-
-void chapter4DivineSolance()
-{
-    printf("Hi");
+    return input;
 }
